7fa39a11509ac469daa57071941abd90a0a36edb
susan1800 <59757247+susan1800@users.noreply.github.com>
2021-07-23 10:35:36 +0545

    Add files via upload

--- js/checkpass.js
+++ js/checkpass.js
@@ -1,88 +1,45 @@
-function myFunction() {
-  var pass1 , pass2;
-  pass1 = document.getElementById("initial").value;
-  pass2 = document.getElementById("second").value;
-  if((pass1==pass2))
-  {
-
-  	document.getElementById('details').innerHTML= 'Password is same';
-  	return false;
-  	
-	}
-
-else
+       function checkpass()
 {
-	if(pass2.length<8)
+    var pass;
+  pass = document.getElementById("pass").value;
+  var len= pass.length
+  if(len<8)
   {
-    if(pass2.length==0)
+    if(len==0)
     {
-      document.getElementById('details').innerHTML= ' ';
-      return false;
+        document.getElementById('msg').innerHTML= ' ';
+        return true;
     }
     else
     {
-      document.getElementById('details').innerHTML= 'password less than 8 character';
-      return false;
+        document.getElementById('msg').innerHTML= 'password less than 8 character';
+        return false;
     }
 
   }
   else
   {
-    document.getElementById('details').innerHTML= ' ';
+    document.getElementById('msg').innerHTML= ' ';
     return true;
   }
-
 }
-}
-// function checkpass()
-// {
-// 	var pass1;
-//   pass1 = document.getElementById("initial").value;
-//   var msg1=$('#check');
-//   var len= pass1.length
-//   if(len<8)
-//   {
-//   	if(pass1.length==0)
-//   	{
-//   		document.getElementById('check').innerHTML= ' ';
-//   		return true;
-//   	}
-//   	else
-//   	{
-//   		document.getElementById('check').innerHTML= '';
-//   		return false;
-//   	}
-
-//   }
-//   else
-//   {
-//   	document.getElementById('check').innerHTML= ' ';
-//   	return true;
-//   }
-// }
 function checksubmit()
 {
-	var x=document.getElementById("initial").value;
-  	var y = document.getElementById("second").value;
-			if((y==""))
-			{
-				return false;
-			}
-			else if(y.length<8)
-			{
-				return false;
-			}
-			else if(y.length>16)
-			{
-				
-				return false;
-			}
-      else if(x==y)
-      {
-        return false;
-      }
-			else
-			{
-				return true;
-			}
+    var x=document.getElementById("pass").value;
+            if((x==""))
+            {
+                return false;
+            }
+            else if(x.length<8)
+            {
+                return false;
+            }
+            else if(x.length>12)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
 }
